dafe0619066a4ad5561fdb669a52bb4338f698ac
Yaraslau Anikovich <yasyamail.ru@mail.ru>
2018-07-06 16:17:41 +0300

    add functional and start localStorage

--- UI/public/js/script.js
+++ UI/public/js/script.js
@@ -209,8 +209,6 @@ var Module = (function () {
         }
     ];
 
-    var tape = [];
-
     function getPosts(filter, skip = 0, top = 10) {
         var result = Array.from(tape);
         // tape.forEach(post => result.push(post));
@@ -245,25 +243,28 @@ var Module = (function () {
 
     function add(photoPost) {
         if (validate(photoPost)) {
-            var date = new Date(photoPost.createdAt);
-            var m = date.getMonth();
-            var d = date.getDate();
-            var y = date.getFullYear();
-            var h = date.getHours();
-            var min = date.getMinutes();
-            var sec = date.getSeconds();
-            var milli = date.getMilliseconds();
+            const tape = JSON.parse(localStorage.getItem('tape'));
+            const date = new Date(photoPost.createdAt);
+            const m = date.getMonth();
+            const d = date.getDate();
+            const y = date.getFullYear();
+            const h = date.getHours();
+            const min = date.getMinutes();
+            const sec = date.getSeconds();
+            const milli = date.getMilliseconds();
             const MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
                 "Jul", "Aug", "Sep", "Oct", "Nov", "De"
             ];
-            var addDate = d + " " + MONTH_NAMES[m] + ' ' + y;
+            const addDate = d + " " + MONTH_NAMES[m] + ' ' + y;
             photoPost.createdAt = addDate;
             if (tape.length === 0) {
                 photoPost.id = '1';
+                tape.push(photoPost);
             } else {
                 photoPost.id = parseInt(tape[tape.length - 1].id) + 1;
+                tape.push(photoPost);
             }
-            //tape.push(tape.length);
+            localStorage.setItem('tape',JSON.stringify(tape));
             return true;
         }
         return false;
@@ -286,8 +287,10 @@ var Module = (function () {
     }
 
     function remove(id) {
-        var length = tape.length;
+        const tape = JSON.parse(localStorage.getItem('tape'));
+        const length = tape.length;
         tape = tape.filter(post => parseInt(post.id) !== parseInt(id));
+        localStorage.setItem('tape', JSON.stringify(tape));
         return length !== tape.length;
     }
 
@@ -299,13 +302,8 @@ var Module = (function () {
         }
     }
 
-    function getT() {
-        return tape;
-    }
-
     return {
         i: 10,
-        tape: tape,
         posts: postsArray,
         getPhotoPosts: getPosts,
         getPhotoPost: getPost,
@@ -313,38 +311,144 @@ var Module = (function () {
         addPhotoPost: add,
         editPhotoPost: edit,
         removePhotoPost: remove,
-        likePhotoPost: like,
-        getTape: getT,
+        likePhotoPost: like
     }
 }());
 
 var Dom = (function () {
-    user = 'Vi';
 
     function initializeStorage(){
+        if(!localStorage.getItem('tape')){
+            const tape = [];
+            localStorage.setItem('tape', JSON.stringify(tape));
+        }
         if(!localStorage.getItem('users')){
-            localStorage.setItem('user', "");
+            const users = [];
+            localStorage.setItem('user', "Kate");
             localStorage.setItem('users', JSON.stringify(users));
-            localStorage.setItem('tape', JSON.stringify(Module.tape));
         }
     }
 
     function redrawPage(){
         var username = localStorage.getItem('user');
-        if(!username){
+        if(username){
             const body = document.getElementsByClassName('body');
-            body.innerHTML = `
-            
+            body[0].innerHTML = `
+            <div class="header">
+            <img class="logo" src="../images/logo.png" alt="">
+            <div class="user">
+                <h3 class="username">
+                    Yaraslau
+                </h3>
+                <button class="exit">
+                  <img src="../images/exit.png" alt="">
+              </button>
+          </div>
+        </div>
+
+        <div class="left-column">
+
+            <form action="" method="" onsubmit="return false;" id="name-search">
+               <h3>Search by Name</h3>
+                <input id="name" type="text" placeholder="Enter name...">
+                <button><img src="../images/loupe.png" alt=""></button>
+            </form>
+
+            <form action="" method="" onsubmit="return false;" id="date-search">
+               <h3>Search by Date</h3>
+               <input id="date" type="text" placeholder="DD-MM-YY">
+                <button><img src="../images/loupe.png" alt=""></button>
+            </form>
+        
+           <form action="" method="" onsubmit="return false;" id="tag-search">
+               <h3>Search by Tag</h3>
+                <input id="tag" type="text" placeholder="#Enter#Tags">
+                <button><img src="../images/loupe.png" alt=""></button>
+            </form>
+        </div>
+        <div class="center-column">
+            <script>
+               Dom.initialization();
+                JSON.parse(localStorage.getItem('tape')).forEach((post) => Dom.drawPhotoPost(post));
+            </script>
+        </div>
+        <div class="right-column">
+           <button class="creating-button" onclick="Dom.addPhotoPost(Module.posts[Module.i++])">
+              Create new post
+          </button>
+        </div>
+
+        <button class="paggination"><img src="../images/reload.png" alt=""></button>
+
+        <div class="footer">
+            <p class="info">Collective_photo_portal_8g_2c ©Created by Yaraslau_Anikovich 01_02_2018</p>
+           <p class="mail">yvanikovich@mail.ru</p>
+        </div>
+
             `;
+            const tape = JSON.parse(localStorage.getItem('tape'));
+            tape.forEach((post) => Dom.drawPhotoPost(post));
         }
     }
 
+    const user = localStorage.getItem('user');
+
+    function draw(post){
+        const posts = document.getElementsByClassName("center-column");
+        let tags = "";
+        let newPost;
+        post.hashTags.forEach(tag => tags += " " + tag);
+        if (user === post.author) {
+            newPost = document.createElement('div');
+            newPost.setAttribute("id", post.id.toString());
+            newPost.setAttribute("class", "post");
+            newPost.innerHTML = `<div class="post-up">
+                    <h3 class="user-name">` + post.author + `</h3>
+                    <h3 class="dateText">` + post.createdAt + `</h3>
+                    <button class="delete-button" onclick="Dom.removePhotoPost(parentNode.parentNode.id.toString())"><img src="../images/x.png" alt="" class="delete-image"
+                    ></button>
+                </div>
+                <img src=` + post.photoLink + ` alt="post-content" class="post-content">
+                <div class="post-down">
+                    <button class="like-button" onclick="Dom.likePhotoPost(parentNode.parentNode.id.toString())">
+                        <img src="../images/not_liked.png" alt="heart" class="like-img">
+                    </button>
+                    <button class="edit-button" onclick="Dom.editPhotoPost(parentNode.parentNode.id.toString())">
+                        <img src="../images/pencil.png" alt="pencil" class="edit-img">
+                    </button>
+                    <h3 class="post-comment">` + post.description + `</h3>
+                    <h3 class="tags">` + tags + `</h3>
+                </div>`;
+        } else {
+            newPost = document.createElement('div');
+            newPost.setAttribute("class", "post");
+            newPost.setAttribute("id", post.id.toString());
+            newPost.innerHTML = `<div class="post-up">
+                    <h3 class="user-name">` + post.author + `</h3>
+                    <h3 class="dateText">` + post.createdAt + `</h3>
+                </div>
+                <img src=` + post.photoLink + ` alt="post-content" class="post-content">
+                <div class="post-down">
+                    <button class="like-button" onclick="Dom.likePhotoPost(parentNode.parentNode.id.toString())">
+                        <img src="../images/not_liked.png" alt="heart" class="like-img">
+                    </button>
+                    <h3 class="post-comment">` + post.description + `</h3>
+                    <h3 class="tags">` + tags + `</h3>
+                </div>`;
+        }
+        const postLike = newPost.childNodes[4].childNodes[1].childNodes[1];
+        if (post.likes.find(usr => usr === user)) {
+            post.likes.push(user);
+            postLike.setAttribute("src", "../images/liked.png");
+        }
+        posts[0].insertBefore(newPost, posts[0].childNodes[0]);
+    }
+
     function add(post) {
-        var newPost = true;
+        let newPost = true;
         if (Module.addPhotoPost(post)) {
-            Module.tape.push(post);
-            var posts = document.getElementsByClassName("center-column");
-            var tags = "";
+            const posts = document.getElementsByClassName("center-column");
+            let tags = "";
             post.hashTags.forEach(tag => tags += " " + tag);
             if (user === post.author) {
                 newPost = document.createElement('div');
@@ -413,24 +517,27 @@ var Dom = (function () {
     }
 
     function remove(id) {
-        var length1 = Module.tape.length;
-        Module.tape = Module.tape.filter(post => parseInt(post.id) !== parseInt(id));
-        var length2 = Module.tape.length;
+        let tape = JSON.parse(localStorage.getItem('tape'));
+        const length1 = tape.length;
+        tape = tape.filter(post => parseInt(post.id) !== parseInt(id));
+        const length2 = tape.length;
         if (length1 !== length2) {
             var element = document.getElementById(id);
             element.remove();
         }
+        localStorage.setItem('tape', JSON.stringify(tape));
     }
 
     function like(id) {
-        if (Module.tape.find(post => parseInt(post.id) === parseInt(id))) {
+        const tape = JSON.parse(localStorage.getItem('tape'));
+        if (tape.find(post => parseInt(post.id) === parseInt(id))) {
             var postLike = document.getElementById(id).childNodes[4].childNodes[1].childNodes[1];
-            Module.tape.forEach(post => {
+            tape.forEach(post => {
                 if (parseInt(post.id) === parseInt(id)) {
                     if (post.likes.length === 0 || !post.likes.find(usr => usr === user)) {
                         post.likes.push(user);
                         postLike.setAttribute("src", "../images/liked.png");
-                        Module.tape.find(post => parseInt(post.id) == parseInt(id)).likes.push;
+                        tape.find(post => parseInt(post.id) == parseInt(id)).likes.push;
                     } else {
                         post.likes = post.likes.filter(el => el !== user);
                         postLike.setAttribute("src", "../images/not_liked.png");
@@ -438,15 +545,19 @@ var Dom = (function () {
                 }
             })
         }
+        localStorage.setItem('tape', JSON.stringify(tape));
 
     }
 
     return {
         user: user,
+        drawPhotoPost: draw,
         addPhotoPost: add,
         editPhotoPost: edit,
         removePhotoPost: remove,
         likePhotoPost: like,
+        initialization: initializeStorage,
+        redraw: redrawPage,
     }
 }());
 
